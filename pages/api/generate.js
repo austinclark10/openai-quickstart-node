a60c1398c90d6e7a8293bc02747ec1c29a257560
austinclark10 <121807250+austinclark10@users.noreply.github.com>
2023-01-02 11:52:54 -0600

    tEST

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.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,
 });
 const openai = new OpenAIApi(configuration);
 
